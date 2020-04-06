7f240374ffc3c822e34bc36eba00a81ccfea328a
Krista Rutz <rutzkri000@gmail.com>
2020-04-06 11:20:03 -0700

    Add ViewModels to facilitate registration/login
    Co-authored-by: Andriy Veremyeyev <belyybrat@gmail.com>

---
+++ ZineClient/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZineClient.ViewModels
+{
+  public class RegisterViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+
+    [Display(Name = "First name")]
+    public string FirstName { get; set; }
+
+    [Display(Name = "Last name")]
+
+    public string LastName { get; set; }
+  }
+}
