ecc8d152b45e1b30f952324dae473a1815dac4aa
Krista Rutz <rutzkri000@gmail.com>
2020-04-07 09:27:48 -0700

    Add routes/views to see complete Tag list/details
    Co-authored-by: Andriy Veremyeyev <belyybrat@gmail.com>

---
+++ ZineClient/Controllers/TagsController.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Authorization;
+using ZineClient.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System;
+
+namespace ZineClient.Controllers
+{
+  public class TagsController : Controller
+  {
+    private readonly ZineClientContext _db;
+    public TagsController(ZineClientContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Tag> model = _db.Tags.ToList();
+      return View(model);
+    }
+
+    // public ActionResult Create()
+    // {
+    //   ViewBag.ZineId = new SelectList(_db.Zines, "ZineId", "Name");
+    //   return View();
+    // }
+
+    // [HttpPost]
+    // public async Task<ActionResult> Create(Post post, int ZineId)
+    // {
+    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //   var currentUser = await _userManager.FindByIdAsync(userId);
+    //   post.ApplicationUser = currentUser;
+    //   post.Published = DateTime.Now;
+
+    //   _db.Posts.Add(post);
+    //   if (ZineId != 0)
+    //   {
+    //     _db.PostZine.Add(new PostZine() { ZineId = ZineId, PostId = post.PostId });
+    //   }
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Details", "Zines", new { id = ZineId });
+    // }
+    public ActionResult Details(int id)
+    {
+      var thisTag = _db.Tags
+      .Include(tag => tag.Posts)
+      .ThenInclude(join => join.Post)
+      .FirstOrDefault(o => o.TagId == id);
+
+      return View(thisTag);
+    }
+
+    // public ActionResult Edit(int id)
+    // {
+    //   var thisOrganization = _db.Organizations.FirstOrDefault(organization => organization.OrganizationId == id);
+
+    //   return View(thisOrganization);
+    // }
+
+    // [HttpPost]
+    // public ActionResult Edit(Organization organization)
+    // {
+    //   _db.Entry(organization).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+    // public ActionResult CheckDelete(int id)
+    // {
+    //   var thisOrganization = _db.Organizations.FirstOrDefault(organization => organization.OrganizationId == id);
+
+    //   return View("Delete", thisOrganization);
+    // }
+
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisPost = _db.Posts.FirstOrDefault(posts => posts.PostId == id);
+
+    //   return View(thisPost);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisPost = _db.Posts.FirstOrDefault(posts => posts.PostId == id);
+    //   _db.Posts.Remove(thisPost);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+  }
+}
